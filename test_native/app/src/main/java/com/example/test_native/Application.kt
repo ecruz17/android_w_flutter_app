ce784b13c2a84d5fc241ed2171d662e81f432dfc
ecruz17 <emircruz.m@gmail.com>
2023-10-24 12:09:27 -0600

    initial commit

---
+++ test_native/app/src/main/java/com/example/test_native/Application.kt
@@ -0,0 +1,31 @@
+package com.example.test_native
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class TestNativeApplication : Application() {
+    lateinit var flutterEngine: FlutterEngine
+
+    companion object {
+        const val FLUTTER_ENGINE_NAME = "module_flutter_engine_name"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put(FLUTTER_ENGINE_NAME, flutterEngine)
+    }
+}
